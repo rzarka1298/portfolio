eb04bcae02829fc6e3cbc90f5eaabfa4b5424ed4
Rugved Zarkar <rugvedzarkar@Rugveds-MacBook-Pro-2.local>
2025-03-02 23:04:36 -0500

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 const App = () => {
   return (
